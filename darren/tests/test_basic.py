70dbbb11a658d7d8a084379a402faa96472a0686
1008tommy <darrenchor1832@gmail.com>
2026-08-21 15:58:53 +0800

    Pytest

--- darren/tests/test_basic.py
+++ darren/tests/test_basic.py
@@ -1,2 +1,23 @@
-def test_basic():
-    assert 1 + 1 == 2
+from pathlib import Path
+
+
+def test_config_exists():
+    assert Path("darren/conf/config.yaml").exists()
+
+
+def test_raw_data_dvc_exists():
+    assert Path("darren/data/raw/global_ai_jobs.csv.dvc").exists()
+
+
+def test_processed_data_dvc_exists():
+    assert Path(
+        "darren/data/processed/global_ai_jobs_preprocessed.csv.dvc"
+    ).exists()
+
+
+def test_notebooks_folder_exists():
+    assert Path("darren/notebooks").exists()
+
+
+def test_models_folder_exists():
+    assert Path("darren/models").exists()
